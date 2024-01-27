92f27ced40f4ec94cbfe2550b3832d33868a5bf3
hetdrisk <het.patel@drisk.io>
2024-01-27 12:45:43 +0530

    Implemented authentication using clerk

--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RegisterPage = () => {
-  return (
-    <div>register page</div>
-  )
-}
-
-export default RegisterPage;
--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,5 +1,7 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <p>Het Patel</p>
+    <UserButton afterSignOutUrl="/"/>
   );
 }
