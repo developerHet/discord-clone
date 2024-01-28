534f46755223a7abb55630a541c6581b2e6e7ff0
hetdrisk <het.patel@drisk.io>
2024-01-28 11:01:36 +0530

    implement server image upload

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import {redirect} from "next/navigation";
+import { redirect } from "next/navigation";
 import { initialProfile } from '@/lib/initial-profile';
 import { db } from '@/lib/db';
+import { InitialModal } from '@/components/modals/initial-modal';
 
 const SetupPage = async () => {
     const profile = await initialProfile();
 
     const server = await db.server.findFirst({
-        where:{
+        where: {
             members: {
                 some: {
                     profileId: profile.id
@@ -16,13 +17,11 @@ const SetupPage = async () => {
         }
     })
 
-    if(server) {
+    if (server) {
         return redirect(`/servers/${server.id}`);
     }
 
-    return (
-        <div>Create a Server</div>
-    )
+    return <InitialModal />
 }
 
 export default SetupPage;
